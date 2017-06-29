4046d59e5bf1971d274e651437684cac7fa0432e
zoumiaojiang <zoumiaojiang@gmail.com>
2017-06-29 17:54:39 +0800

    code style

--- test/helpers/setupLavas.js
+++ test/helpers/setupLavas.js
@@ -3,17 +3,15 @@
  * @author mj(zoumiaojiang@gmail.com)
  */
 
-
 import lavas from '../../index';
 import path from 'path';
-
 
 async function setupLavas() {
     // const schema = await lavas.getSchema();
 
     // console.log(schema.properties);
     // console.log('start time: ', Date.now());
-    const ret = await lavas.exportProject({
+    let ret = await lavas.exportProject({
         name: 'xxxx',
         author: 'zoumiaojiang',
         email: 'zoumiaojiang@gmail.com'
